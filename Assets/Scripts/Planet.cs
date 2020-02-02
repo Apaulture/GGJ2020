dc392163d34d3fe2d19d08c43c5c38e5f9ad3025
IoriBranford <ioribranford@gmail.com>
2020-02-01 16:57:39 -0800

    Simple planet with health. Shrink when hit by a meteor and grow when hit by (for now) anything else.

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -5,23 +5,41 @@ using UnityEngine;
 public class Planet : MonoBehaviour
 {
 	const int NUM_SLICES = 8;
+	public int MaxHealth = 10;
+	public int Health = 5;
+	public float ScalePerHealth = .25f;
 
 	void Start () {
-		for (int i = 0; i < NUM_SLICES; ++i) {
-			int broken = Random.Range(0, 3);
-			if (broken < 2) {
-				Transform layer = gameObject.transform.Find("Layer3");
-				Transform block = layer.GetChild(i);
-				block.gameObject.SetActive(false);
-			}
-			if (broken < 1) {
-				Transform layer = gameObject.transform.Find("Layer2");
-				Transform block = layer.GetChild(i);
-				block.gameObject.SetActive(false);
-			}
-		}
+		SetScale(Health*ScalePerHealth);
 	}
 
 	void Update() {
+	}
+
+	void SetScale (float s) {
+		var scale = transform.localScale;
+		scale.x = scale.y = scale.z = s;
+		transform.localScale = scale;
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (Health >= MaxHealth)
+			return;
+
+		if (other.gameObject.tag == "Meteor") {
+			--Health;
+		} else /*TODO tag for healing object if needed*/ {
+			++Health;
+		}
+		if (Health >= MaxHealth) {
+			Health = MaxHealth;
+			// Restored, victory
+		}
+		SetScale(Health * ScalePerHealth);
+		if (Health <= 0) {
+			Destroy(gameObject);
+			//Explode, game over
+		}
+		Destroy(other.gameObject);
 	}
 }
