b96b7b5957cee7dfc74684f775a19ed930fe1c76
Apaulture <apaulture@gmail.com>
2020-02-02 13:41:05 -0800

    - Added 2 scenes, main menu and briefing - Transitions between menu

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+    public Button yourButton;
+    public float waitTime;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+        // stay cool
+        if (SceneManager.GetActiveScene().name == "Briefing")
+        {
+            StartCoroutine(Wait());
+        }
+    }
+
+    // Main menu to briefing
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(sceneName: "Briefing");
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(sceneName: "Game");
+    }
+}
