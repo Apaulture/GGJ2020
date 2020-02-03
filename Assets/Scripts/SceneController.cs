9c98a594cedde39791274f0710e597e054206b6f
carlos <canoguez@icloud.com>
2020-02-02 16:27:56 -0800

    - Added scenes and scene transitions - Added audio to briefing

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -6,30 +6,54 @@ using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
-    public Button yourButton;
-    public float waitTime;
+    public Button playButton;
+    public Button howToButton;
+    public Button creditsButton;
+
+    public float logoMenuWait;
+    public float briefingGameWait;
 
     void Start()
     {
-        Button btn = yourButton.GetComponent<Button>();
-        btn.onClick.AddListener(TaskOnClick);
+        Button pButton = playButton.GetComponent<Button>();
+        Button hwButton = howToButton.GetComponent<Button>();
+        Button cButton = creditsButton.GetComponent<Button>();
 
-        // stay cool
-        if (SceneManager.GetActiveScene().name == "Briefing")
-        {
-            StartCoroutine(Wait());
-        }
+        pButton.onClick.AddListener(Play);
+        hwButton.onClick.AddListener(HowToPlay);
+        cButton.onClick.AddListener(Credits);
+
+        StartCoroutine(Wait());
     }
 
     // Main menu to briefing
-    void TaskOnClick()
+    void Play()
     {
         SceneManager.LoadScene(sceneName: "Briefing");
     }
 
+    void HowToPlay()
+    {
+        SceneManager.LoadScene(sceneName: "Howtoplay");
+    }
+
+    void Credits()
+    {
+        SceneManager.LoadScene(sceneName: "Credits");
+    }
+
     IEnumerator Wait()
     {
-        yield return new WaitForSeconds(waitTime);
-        SceneManager.LoadScene(sceneName: "Game");
+        if (SceneManager.GetActiveScene().name == "Briefing")
+        {
+            yield return new WaitForSeconds(briefingGameWait);
+            SceneManager.LoadScene(sceneName: "Game");
+        }
+        // Logo to main menu scene transition
+        else if (SceneManager.GetActiveScene().name == "Logo")
+        {
+            yield return new WaitForSeconds(logoMenuWait);
+            SceneManager.LoadScene(sceneName: "Main Menu");
+        }
     }
 }
