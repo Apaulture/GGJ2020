3fe28dbb4a88535e315fdebb4d8dac63e7c8212a
IoriBranford <ioribranford@gmail.com>
2020-02-03 22:47:49 -0800

    Tutorial. Don't spawn any meteors until end of tutorial.

--- Assets/Scripts/MeteorController.cs
+++ Assets/Scripts/MeteorController.cs
@@ -1,42 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MeteorController : MonoBehaviour
 {
-    public GameObject meteor;
-    public int meteorNum;
-    public int velocityMultiplier;
-    public float waitTime;
+	public GameObject meteor;
+	public int meteorNum;
+	public int velocityMultiplier;
+	public float waitTime;
 
-    GameObject spawnedMeteor;
+	GameObject spawnedMeteor;
+	public bool started = false;
 
-    void Start()
-    {
-        StartCoroutine(TimeBetweenMeteors());
-    }
+	public void StartMeteors()
+	{
+		if (!started) {
+			started = true;
+			StartCoroutine(TimeBetweenMeteors());
+			var tutorialText = GameObject.FindObjectOfType<TutorialText>();
+			if (tutorialText) {
+				tutorialText.SetText("");
+			}
+		}
+	}
 
-    private void Update()
-    {
-    }
+	IEnumerator TimeBetweenMeteors()
+	{
+		while (transform.childCount < meteorNum)
+		{
+			float randomTheta = Random.value * Mathf.PI * 2;
+			float randomX = Mathf.Cos(randomTheta) * 15;
+			float randomZ = Mathf.Sin(randomTheta) * 15;
 
-    IEnumerator TimeBetweenMeteors()
-    {
-        while (transform.childCount < meteorNum)
-        {
-            float randomTheta = Random.value * Mathf.PI * 2;
-            float randomX = Mathf.Cos(randomTheta) * 15;
-            float randomZ = Mathf.Sin(randomTheta) * 15;
+			Vector3 randomPosition = new Vector3(randomX, 0, randomZ);
+			spawnedMeteor = Instantiate(meteor, randomPosition, Quaternion.identity, transform);
 
-            Vector3 randomPosition = new Vector3(randomX, 0, randomZ);
-            spawnedMeteor = Instantiate(meteor, randomPosition, Quaternion.identity, transform);
+			Vector3 velocity = (-randomPosition).normalized;
+			spawnedMeteor.GetComponent<Rigidbody>().velocity = velocity * velocityMultiplier;
 
-            Vector3 velocity = (-randomPosition).normalized;
-            spawnedMeteor.GetComponent<Rigidbody>().velocity = velocity * velocityMultiplier;
+			waitTime -= .015f;
 
-            waitTime -= .015f;
-
-            yield return new WaitForSeconds(waitTime);
-        }
-    }
+			yield return new WaitForSeconds(waitTime);
+		}
+	}
 }
---
+++ Assets/Scripts/TutorialText.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialText : MonoBehaviour
+{
+	public void SetText(string text) {
+		GetComponent<Text>().text = text;
+	}
+}
