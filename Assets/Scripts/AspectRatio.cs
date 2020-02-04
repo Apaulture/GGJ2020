9f4a0fa81a65521f1f13324ebb0835b875beb96f
IoriBranford <ioribranford@gmail.com>
2020-02-03 18:31:52 -0800

    Handle aspect ratio other than 1:1.

---
+++ Assets/Scripts/AspectRatio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AspectRatio : MonoBehaviour
+{
+	void Start() {
+		var camera = GetComponent<Camera>();
+		var rect = camera.pixelRect;
+		print(rect);
+		var letterboxWidth = (rect.width - rect.height)/2;
+		if (rect.width < rect.height) {
+			letterboxWidth = -letterboxWidth;
+			rect.y = letterboxWidth;
+			rect.height = rect.width;
+		} else {
+			rect.x = letterboxWidth;
+			rect.width = rect.height;
+		}
+		camera.pixelRect = rect;
+	}
+}
